b344f48af54d95d1f6f1543afa5d2b2eaf4423e4
ala <alaamamlouk09@gmail.com>
2023-12-11 09:54:39 +0100

    first commit

---
+++ http/server.js
@@ -0,0 +1,20 @@
+var http=require("http");
+var server=http.createServer(function(req,res){
+    res.writeHead(200,{"Content-Type":"text/html"});
+    res.end(`<!DOCTYPE html>
+             <html>   
+                <head>
+                <title>HTML response </title>
+                </head>
+                <body>
+                <h1>Serving HTML text</h1>
+                <p>${req.url}</p>
+                <p>${req.method}</p>
+                </body>
+                </html>
+
+    
+    `);
+});
+server.listen(3000);
+console.log("Server listening on port 3000");
